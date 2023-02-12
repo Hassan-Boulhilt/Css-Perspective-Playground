bada3b27fc81fa62f203346c25401d121e10fb79
hassan-boulhilt <hboulhilt@gmail.com>
2023-02-12 23:33:30 +0100

    Working with keyboard modifier like .enter or .ctrl

--- Vue-Fundamentals/app.js
+++ Vue-Fundamentals/app.js
@@ -2,6 +2,7 @@ const vm = Vue.createApp({
     data(){
         return {
             firstName:'Hassan',
+            middleName:'',
             lastName:"Deo",
             url_g:"https://www.google.com",
             url_b:"https://www.bing.com",
@@ -11,12 +12,15 @@ const vm = Vue.createApp({
     },
     methods:{
         getFullName(){
-            return `${this.firstName} ${this.lastName.toUpperCase()}`
+            return `${this.firstName} ${this.middleName} ${this.lastName.toUpperCase()}`
         },
         updateFirstName(msg, event){
             
             console.log(msg)
             this.firstName = event.target.value
+        },
+        updateMiddleName(event){
+            this.middleName = event.target.value 
         },
         incrementeAge(){
             this.age++
