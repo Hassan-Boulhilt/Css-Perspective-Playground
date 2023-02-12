97b78737e84060e3f08b80c0d5f2280c6af179ca
hassan-boulhilt <hboulhilt@gmail.com>
2023-02-12 20:23:39 +0100

    first commit

---
+++ Vue-Fundamentals/app.js
@@ -0,0 +1,28 @@
+const vm = Vue.createApp({
+    // data(){
+    //     return {
+    //         firstName:'Hassan',
+    //         lastName:"Deo",
+    //         url:"https://www.google.com",
+    //         row_url:'<a href="http://www.google.com" target="_blanc">Google</a>',
+    //         age:20,
+    //     }
+    // },
+    // methods:{
+    //     getFullName(){
+    //         return `${this.firstName} ${this.lastName.toUpperCase()}`
+    //     },
+    //     increment(){
+    //         this.age++
+    //     },
+    //     updateLastName(event){
+    //         this.lastName = event.target.value
+    //     }
+
+    // }
+
+}).mount('#app')
+
+// setTimeout(()=>{
+//   vm.firstName='Mohamed'
+// },2000);
