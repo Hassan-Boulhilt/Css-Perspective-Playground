83e8053e2a68dfa9cb0e546815eda944128502ef
hassan-boulhilt <hboulhilt@gmail.com>
2023-02-12 20:25:18 +0100

    first commit

--- Vue-Fundamentals/app.js
+++ Vue-Fundamentals/app.js
@@ -1,27 +1,27 @@
-const vm = Vue.createApp({
-    // data(){
-    //     return {
-    //         firstName:'Hassan',
-    //         lastName:"Deo",
-    //         url:"https://www.google.com",
-    //         row_url:'<a href="http://www.google.com" target="_blanc">Google</a>',
-    //         age:20,
-    //     }
-    // },
-    // methods:{
-    //     getFullName(){
-    //         return `${this.firstName} ${this.lastName.toUpperCase()}`
-    //     },
-    //     increment(){
-    //         this.age++
-    //     },
-    //     updateLastName(event){
-    //         this.lastName = event.target.value
-    //     }
+// const vm = Vue.createApp({
+//     // data(){
+//     //     return {
+//     //         firstName:'Hassan',
+//     //         lastName:"Deo",
+//     //         url:"https://www.google.com",
+//     //         row_url:'<a href="http://www.google.com" target="_blanc">Google</a>',
+//     //         age:20,
+//     //     }
+//     // },
+//     // methods:{
+//     //     getFullName(){
+//     //         return `${this.firstName} ${this.lastName.toUpperCase()}`
+//     //     },
+//     //     increment(){
+//     //         this.age++
+//     //     },
+//     //     updateLastName(event){
+//     //         this.lastName = event.target.value
+//     //     }
 
-    // }
+//     // }
 
-}).mount('#app')
+// }).mount('#app')
 
 // setTimeout(()=>{
 //   vm.firstName='Mohamed'
