6a422f30e6f494a97284c76e938fdef6c16ff5ba
hassan-boulhilt <hboulhilt@gmail.com>
2023-02-12 22:24:30 +0100

    Working with events v-on:click='' == @click='' @input ...

--- Vue-Fundamentals/app.js
+++ Vue-Fundamentals/app.js
@@ -5,12 +5,19 @@ const vm = Vue.createApp({
             lastName:"Deo",
             url_g:"https://www.google.com",
             url_b:"https://www.bing.com",
-            raw_url:'<a href="https://www.youtube.com" target="_blanc">You tube</a>'
+            raw_url:'<a href="https://www.youtube.com" target="_blanc">You tube</a>',
+            age:20,
         }
     },
     methods:{
         getFullName(){
             return `${this.firstName} ${this.lastName.toUpperCase()}`
+        },
+        updateFirstName(event){
+            this.firstName = event.target.value
+        },
+        incrementeAge(event){
+            this.age++
         }
     }
     
