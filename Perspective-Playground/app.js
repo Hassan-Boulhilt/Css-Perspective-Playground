036657cf1746a456c91225fa39c73f4b7c466b8e
hassan-boulhilt <hboulhilt@gmail.com>
2023-02-16 00:35:47 +0100

    add perspective playground small project

---
+++ Perspective-Playground/app.js
@@ -0,0 +1,46 @@
+let vm = Vue.createApp({
+    data(){
+        return{
+            perspective_value:499,
+            rotateX_value:0,
+            rotateY_value:0,
+            rotateZ_value:0,
+        }
+    },
+    methods:{
+        reset(){
+            this.perspective_value=499;
+            this.rotateX_value=0;
+            this.rotateY_value=0;
+            this.rotateZ_value=0;
+
+        }
+
+    },
+    computed:{
+        
+        box (){
+            
+            
+                return{
+                    transform:`perspective(${this.perspective_value}px) rotateX(${this.rotateX_value}deg) rotateY(${this.rotateY_value}deg) rotateZ(${this.rotateZ_value}deg)`,
+                    
+                }
+                
+           
+        },
+       
+                
+         
+
+        
+    },watch:{
+
+       
+        
+
+
+    }
+   
+
+}).mount('#app')
